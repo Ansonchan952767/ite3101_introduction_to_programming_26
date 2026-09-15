1c0104ea89fd88cad3d7d0d667111883db609224
Ansonchan <monkeyking9527927@gmail.com>
2026-09-15 02:06:30 +0000

    [skip actions] lab/lab02/ch02_t02_practice.py

--- lab/lab02/ch02_t02_practice.py
+++ lab/lab02/ch02_t02_practice.py
@@ -1,6 +1,6 @@
 # Assign your variables below, each on its own line!
 caesar ="Graham"
-pa"John"
+pra= "John"
 "Teresa"
 # Put your variables above this line, and uncomment the following 3 line of codes.
 # print(caesar)
