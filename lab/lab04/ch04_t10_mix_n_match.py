8929674a98ccbb9517922dc866166e7d48735519
Ansonchan <monkeyking9527927@gmail.com>
2026-09-22 03:19:45 +0000

    [skip actions] lab/lab04/ch04_t10_mix_n_match.py

--- lab/lab04/ch04_t10_mix_n_match.py
+++ lab/lab04/ch04_t10_mix_n_match.py
@@ -7,7 +7,7 @@ bool_one = (2 <= 2) and "Alpha" == "Bravo"  # We did this one for you!
 bool_two = not (3>5) or (10 == 10)
 
 # Make me false!
-bool_three = None
+bool_three = (5)
 
 # Make me true!
 bool_four = None
